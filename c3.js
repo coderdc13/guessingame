bf7b8361719e61bf82f90602374dda13bd796628
coderdc13 <franklin.rivera.bueno@gmail.com>
2018-12-14 21:23:11 -0500

    first guessing game commit

--- c3.js
+++ c3.js
@@ -1,55 +1,43 @@
 
-var guess = null;
 
-var message = document.getElementById("message");
+var guess = null
+var message = document.getElementById("message")
 var value = Math.floor(Math.random() * 100);
+console.log("Value: ", value)
 var guessCount = 0;
-var guessLimit = 1;
-
-
-var guessLimit = 10;
-console.log(value);
-
+var guessLimit = 13;
+console.log(value)
 setNumber = (g) => {
-    guess = parseInt(g.value);
+    guess = parseInt(g.value)
 }
-
 check = (numberOfGuesses) => {
-    console.log("Hello ==>>", numberOfGuesses)
-    if (numberOfGuesses > guessLimit) {
-        message.innerHTML = "too many guesses";
-        return false;
-        // document.getElementById("guessButton").disabled = true;
-    }
-        return true;
+    if (numberOfGuesses >= guessLimit) {
+    message.innerHTML = "too many guesses"
+    return false
+  }
+  return true
 }
-
-check = (numberOfGuesses) => {};
-generateList = (arrayOfGuesses) => {
-    return arrayOfGuesses.map((g) => {
-       return "<li>" + g + "</li>"
-    })
-    var newList = []
-    for(i = 0; i < arrayOfGuesses.length; i++) {
-        elem = arrayOfGuesses[i]
-        newList.push("<li>" + elem + "<li>")
-    }
-    return newList;
+reset = () => {
+    guessCount = 0
+  guess = 0
+  value = Math.floor(Math.random() * 100)
+  console.log("Value: ", value)
+  document.getElementById("guess").value = ""
+  document.getElementById("message").innerHTML = ""
 }
-
 submit = () => {
-    guess.push(guess)
-    document.getElementById("guesses").innerHTML = generateList(guesses).join(" ") 
-    // aboce special note for something with three steps...
+  if (check(guessCount)) {
     if (guess === value) {
-        message.innerHTML = "success";
-        value = Math.floor(Math.random() * 100);
-        guessCount = 0;
+      message.innerHTML = "success"
+      value = Math.floor(Math.random() * 100);
+      console.log("Value: ", value)
+      guessCount = 0
     } else if (guess < value) {
-        message.innerHTML = "too low";
-        guessCount = guessCount + 1;
-    } else  {
-        message.innerHTML = "too high";
-        guessCount = guessCount + 1;
+      message.innerHTML = "too low"
+      guessCount = guessCount + 1
+    } else {
+      message.innerHTML = "too high"
+      guessCount = guessCount + 1
     }
+  }
 }
